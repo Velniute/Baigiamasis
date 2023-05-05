784d7b656c69b1047a9f836056ca47910d220b73
RedaS <svelniute@yahoo.com>
2023-05-05 14:15:55 +0300

    sestas commitas

--- Autobilis_duomenu_tvarkymas.py
+++ Autobilis_duomenu_tvarkymas.py
@@ -16,21 +16,16 @@ for col in df.columns:
 
 df['Metai'] = df['Metai'].str.replace('-\s\d+', '', regex=True).str.slice(stop=4)
 
-
 for col in ['Rida', 'Variklio_galia']:
     df[col] = df[col].apply(lambda x: int(x) if str(x).isdigit() else 0)
 
-
 df['Metai'] = pd.to_datetime(df['Metai'].str[:4], format='%Y')
 
-
 df['Kaina'] = df['Kaina'].astype(int)
-
 
 df = df.rename(columns={'Variklio_galia': 'Variklio_galia_kW', 'Rida': 'Rida_km', 'Kaina': 'Kaina_Eur.'})
 
 df.info()
-
 
 df_clean = df.copy()
 
@@ -40,7 +35,6 @@ for col in ['Kebulo_tipas', 'Kuro_tipas', 'Pavaru_dezes_tipas']:
     klaidos_col = df_clean[skaicius][col].tolist()
     klaidos += klaidos_col
     print(f"{len(klaidos_col)} netinkamos reikšmės stulpelyje {col}")
-
 
 df_clean = df_clean[~df_clean.isin(klaidos)].dropna()
 print(f"Po išmetimo, DataFrame yra dydžio {df_clean.shape}")
